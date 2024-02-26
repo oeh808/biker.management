038f4c33e088f47755d8f314cb34e766e53f0638
OEH808 <omarehababdelaziz8@gmail.com>
2024-02-26 14:19:36 +0200

    Changed column ordering for bikers

--- src/main/resources/db/migration/V1__Create_users_tables.sql
+++ src/main/resources/db/migration/V1__Create_users_tables.sql
@@ -9,5 +9,5 @@ create table back_office_users (
 );
 
 create table bikers (
-    id integer not null, current_location varchar(255), email varchar(255) unique, name varchar(255), phone_number varchar(255) unique, primary key (id)
+    id integer not null, email varchar(255) unique, name varchar(255), phone_number varchar(255) unique, current_location varchar(255), primary key (id)
 );
