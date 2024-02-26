dd5b70b93d165acb333017bf3eea1f7391fa1be3
OEH808 <omarehababdelaziz8@gmail.com>
2024-02-26 14:40:04 +0200

    Updated roles to shorten them

--- src/main/java/io/biker/management/auth/Roles.java
+++ src/main/java/io/biker/management/auth/Roles.java
@@ -1,9 +1,9 @@
 package io.biker.management.auth;
 
 public class Roles {
-    public static final String ADMIN = "ROLE_ADMIN";
-    public static final String BACK_OFFICE = "ROLE_BACK_OFFICE";
-    public static final String STORE = "ROLE_STORE";
-    public static final String BIKER = "ROLE_BIKER";
-    public static final String CUSTOMER = "ROLE_CUSTOMER";
+    public static final String ADMIN = "ADMIN";
+    public static final String BACK_OFFICE = "BACK_OFFICE";
+    public static final String STORE = "STORE";
+    public static final String BIKER = "BIKER";
+    public static final String CUSTOMER = "CUSTOMER";
 }
