ff7df91c7f11ffb80a1e3f676d1f1b2d3db0904c
OEH808 <omarehababdelaziz8@gmail.com>
2024-02-26 14:37:36 +0200

    Add a static class for keeping track of roles

---
+++ src/main/java/io/biker/management/auth/Roles.java
@@ -0,0 +1,9 @@
+package io.biker.management.auth;
+
+public class Roles {
+    public static final String ADMIN = "ROLE_ADMIN";
+    public static final String BACK_OFFICE = "ROLE_BACK_OFFICE";
+    public static final String STORE = "ROLE_STORE";
+    public static final String BIKER = "ROLE_BIKER";
+    public static final String CUSTOMER = "ROLE_CUSTOMER";
+}
