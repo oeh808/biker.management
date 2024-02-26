d9d88c88660d6479ccefb8d634eeb72c0a92c12a
OEH808 <omarehababdelaziz8@gmail.com>
2024-02-26 14:19:56 +0200

    Created skeletons for service

---
+++ src/main/java/io/biker/management/back_office/service/BackOfficeServiceImpl.java
@@ -0,0 +1,36 @@
+package io.biker.management.back_office.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import io.biker.management.back_office.entity.BackOfficeUser;
+
+@Service
+public class BackOfficeServiceImpl implements BackOfficeService {
+
+    @Override
+    public BackOfficeUser createBackOfficeUser(BackOfficeUser boUser) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createBackOfficeUser'");
+    }
+
+    @Override
+    public List<BackOfficeUser> getAllBackOfficeUsers() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllBackOfficeUsers'");
+    }
+
+    @Override
+    public BackOfficeUser getSingleBackOfficeUser(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSingleBackOfficeUser'");
+    }
+
+    @Override
+    public String deleteBackOfficeUser(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteBackOfficeUser'");
+    }
+
+}
---
+++ src/main/java/io/biker/management/biker/service/BikerServiceImpl.java
@@ -0,0 +1,36 @@
+package io.biker.management.biker.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import io.biker.management.biker.entity.Biker;
+
+@Service
+public class BikerServiceImpl implements BikerService {
+
+    @Override
+    public Biker createBiker(Biker biker) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createBiker'");
+    }
+
+    @Override
+    public List<Biker> getAllBikers() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllBikers'");
+    }
+
+    @Override
+    public Biker getSingleBiker(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSingleBiker'");
+    }
+
+    @Override
+    public String deleteBiker(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteBiker'");
+    }
+
+}
