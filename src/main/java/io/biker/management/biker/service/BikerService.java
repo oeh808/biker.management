2e538d63c57661fb9a6c2062ebf9d3a2478c9632
OEH808 <omarehababdelaziz8@gmail.com>
2024-02-26 13:59:34 +0200

    Updated method return type

---
+++ src/main/java/io/biker/management/biker/service/BikerService.java
@@ -0,0 +1,15 @@
+package io.biker.management.biker.service;
+
+import java.util.List;
+
+import io.biker.management.biker.entity.Biker;
+
+public interface BikerService {
+    public Biker createBiker(Biker biker);
+
+    public List<Biker> getAllBikers();
+
+    public Biker getSingleBiker(int id);
+
+    public String assignDelivery();
+}
