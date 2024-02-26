61b31e1d4e0c09d13c1f4f1462130cf1da2ae3d1
OEH808 <omarehababdelaziz8@gmail.com>
2024-02-26 14:22:46 +0200

    Created skeleton for auth controller

---
+++ src/main/java/io/biker/management/auth/controller/AuthController.java
@@ -0,0 +1,33 @@
+package io.biker.management.auth.controller;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.biker.management.auth.entity.AuthRequest;
+import io.biker.management.auth.service.JwtService;
+import io.biker.management.auth.service.UserInfoService;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private UserInfoService userinfoService;
+    private JwtService jwtService;
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/generateToken")
+    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(authRequest.getUsername());
+        } else {
+            throw new UsernameNotFoundException("Invalid user request!");
+        }
+    }
+}
