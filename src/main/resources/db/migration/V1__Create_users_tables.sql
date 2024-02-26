4da3ee8f106da4bc6d031b454fcd203d72b0b6a6
OEH808 <omarehababdelaziz8@gmail.com>
2024-02-26 14:29:47 +0200

    Updated migration to include user info used for authentication

--- src/main/resources/db/migration/V1__Create_users_tables.sql
+++ src/main/resources/db/migration/V1__Create_users_tables.sql
@@ -1,13 +1,17 @@
 create sequence user_sequence start with 1 increment by 1;
 
 create table admins (
-    id integer not null, email varchar(255) unique, name varchar(255), phone_number varchar(255) unique, primary key (id)
+    id integer not null, name varchar(255), email varchar(255) unique, phone_number varchar(255) unique, primary key (id)
 );
 
 create table back_office_users (
-    id integer not null, email varchar(255) unique, name varchar(255), phone_number varchar(255) unique, primary key (id)
+    id integer not null, name varchar(255), email varchar(255) unique, phone_number varchar(255) unique, primary key (id)
 );
 
 create table bikers (
-    id integer not null, email varchar(255) unique, name varchar(255), phone_number varchar(255) unique, current_location varchar(255), primary key (id)
+    id integer not null, name varchar(255), email varchar(255) unique, phone_number varchar(255) unique, current_location varchar(255), primary key (id)
+);
+
+create table user_info (
+    id integer not null, username varchar(255), password varchar(255), roles varchar(255), primary key (id)
 );
