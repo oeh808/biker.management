6c024eee853d9a49ff93f0e0b904e3c3dbd322b0
OEH808 <omarehababdelaziz8@gmail.com>
2024-02-27 17:07:01 +0200

    Added customer pathings to security configuration

--- src/main/java/io/biker/management/auth/config/SecurityConfig.java
+++ src/main/java/io/biker/management/auth/config/SecurityConfig.java
@@ -40,13 +40,15 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/auth/bikers", "/auth/generateToken",
+                        .requestMatchers("/auth/bikers", "/auth/customers", "/auth/generateToken",
                                 "/swagger-ui/**", "/api-docs/**")
                         .permitAll())
                 .authorizeHttpRequests(requests -> requests.requestMatchers("/auth/backOffice/**").authenticated())
                 .authorizeHttpRequests(requests -> requests.requestMatchers("/auth/bikers/**").authenticated())
+                .authorizeHttpRequests(requests -> requests.requestMatchers("/auth/customers/**").authenticated())
                 .authorizeHttpRequests(requests -> requests.requestMatchers("/users/**").authenticated())
                 .authorizeHttpRequests(requests -> requests.requestMatchers("/bikers/**").authenticated())
+                .authorizeHttpRequests(requests -> requests.requestMatchers("/customers/**").authenticated())
                 .sessionManagement(management -> management
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider())
