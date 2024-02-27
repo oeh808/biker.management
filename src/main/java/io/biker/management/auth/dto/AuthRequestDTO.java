b3f3c66bfd71adad158fb89a1601299d71e18611
OEH808 <omarehababdelaziz8@gmail.com>
2024-02-27 14:55:09 +0200

    Updated swagger annotation for dtos

--- src/main/java/io/biker/management/auth/dto/AuthRequestDTO.java
+++ src/main/java/io/biker/management/auth/dto/AuthRequestDTO.java
@@ -1,9 +1,14 @@
 package io.biker.management.auth.dto;
 
 import io.biker.management.constants.validation.ValidationMessages;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 public record AuthRequestDTO(
-        @NotBlank(message = ValidationMessages.NOT_BLANK) String username,
-        @NotBlank(message = ValidationMessages.NOT_BLANK) String password) {
+                @Schema(requiredProperties = {
+                                "Cannot be null",
+                                "Cannot be empty" }) @NotBlank(message = ValidationMessages.NOT_BLANK) String username,
+                @Schema(requiredProperties = {
+                                "Cannot be null",
+                                "Cannot be empty" }) @NotBlank(message = ValidationMessages.NOT_BLANK) String password){
 }
--- src/main/java/io/biker/management/auth/dto/UserCreationDTO.java
+++ src/main/java/io/biker/management/auth/dto/UserCreationDTO.java
@@ -1,11 +1,20 @@
 package io.biker.management.auth.dto;
 
 import io.biker.management.constants.validation.ValidationMessages;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 public record UserCreationDTO(
-        @NotBlank(message = ValidationMessages.NOT_BLANK) String name,
-        @NotBlank(message = ValidationMessages.NOT_BLANK) String username,
-        @NotBlank(message = ValidationMessages.NOT_BLANK) String password,
-        @NotBlank(message = ValidationMessages.NOT_BLANK) String phoneNum) {
+                @Schema(requiredProperties = {
+                                "Cannot be null",
+                                "Cannot be empty" }) @NotBlank(message = ValidationMessages.NOT_BLANK) String name,
+                @Schema(requiredProperties = {
+                                "Cannot be null",
+                                "Cannot be empty" }) @NotBlank(message = ValidationMessages.NOT_BLANK) String username,
+                @Schema(requiredProperties = {
+                                "Cannot be null",
+                                "Cannot be empty" }) @NotBlank(message = ValidationMessages.NOT_BLANK) String password,
+                @Schema(requiredProperties = {
+                                "Cannot be null",
+                                "Cannot be empty" }) @NotBlank(message = ValidationMessages.NOT_BLANK) String phoneNum){
 }
