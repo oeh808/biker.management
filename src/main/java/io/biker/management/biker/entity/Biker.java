a1356ea751affc5d2c12056067622d0b1c2b3b14
OEH808 <omarehababdelaziz8@gmail.com>
2024-02-28 11:10:07 +0200

    Returned biker address to String (Address was too much information, can be replaced with a link for navigation)

--- src/main/java/io/biker/management/biker/entity/Biker.java
+++ src/main/java/io/biker/management/biker/entity/Biker.java
@@ -1,6 +1,5 @@
 package io.biker.management.biker.entity;
 
-import io.biker.management.user.Address;
 import io.biker.management.user.entity.User;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -8,22 +7,22 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "BIKERS")
 public class Biker extends User {
-    private Address currentLocation;
+    private String currentLocation;
 
     public Biker() {
         super();
     }
 
-    public Biker(int id, String name, String email, String phoneNumber, Address currentLocation) {
+    public Biker(int id, String name, String email, String phoneNumber, String currentLocation) {
         super(id, name, email, phoneNumber);
         this.currentLocation = currentLocation;
     }
 
-    public Address getCurrentLocation() {
+    public String getCurrentLocation() {
         return currentLocation;
     }
 
-    public void setCurrentLocation(Address currentLocation) {
+    public void setCurrentLocation(String currentLocation) {
         this.currentLocation = currentLocation;
     }
 }
