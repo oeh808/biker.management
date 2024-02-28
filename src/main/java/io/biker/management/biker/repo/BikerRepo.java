19cb3d6c156cad505d79550dc11dec7debe14b09
OEH808 <omarehababdelaziz8@gmail.com>
2024-02-28 10:29:31 +0200

    Removed ununused methods from repos and deleted obsolete tests

--- src/main/java/io/biker/management/biker/repo/BikerRepo.java
+++ src/main/java/io/biker/management/biker/repo/BikerRepo.java
@@ -1,13 +1,8 @@
 package io.biker.management.biker.repo;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import io.biker.management.biker.entity.Biker;
 
 public interface BikerRepo extends JpaRepository<Biker, Integer> {
-    public Optional<Biker> findByPhoneNumber(String phoneNumber);
-
-    public Optional<Biker> findByEmail(String email);
 }
