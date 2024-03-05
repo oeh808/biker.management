497de6420bb37966f9a0275c0362249e3dbb1e75
OEH808 <omarehababdelaziz8@gmail.com>
2024-03-05 14:10:06 +0200

    Updated mapping and location to reflect refined purpose of controller

--- src/main/java/io/biker/management/user/analysis/controller/AnalysisController.java
+++ src/main/java/io/biker/management/user/analysis/controller/AnalysisController.java
@@ -1,4 +1,4 @@
-package io.biker.management.user.controller;
+package io.biker.management.user.analysis.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,40 +20,40 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
-@Tag(name = "Users/Management")
+@Tag(name = "Analysis")
 @SecurityRequirement(name = "Authorization")
-@RequestMapping("/users")
-public class UserController {
+@RequestMapping("/analysis")
+public class AnalysisController {
     private AnalysisServiceImpl analysisService;
     private BikerService bikerService;
 
-    public UserController(AnalysisServiceImpl analysisService, BikerService bikerService) {
+    public AnalysisController(AnalysisServiceImpl analysisService, BikerService bikerService) {
         this.analysisService = analysisService;
         this.bikerService = bikerService;
     }
 
     @Operation(description = "GET endpoint for retrieving an analysis of a biker's performance given their id." +
             "\n\n Can only be done by back office users.", summary = "Get an analysis of a single biker's performance")
-    @GetMapping("/analysis/{id}")
+    @GetMapping("/{id}")
     @PreAuthorize("hasAuthority('" + Roles.BACK_OFFICE + "')")
-    public BikerAnalysis AnalyzeBikerPerformance(
+    public BikerAnalysis analyzeBikerPerformance(
             @Parameter(in = ParameterIn.PATH, name = "id", description = "Biker ID") @PathVariable int id) {
         return analysisService.getBikerAnalysis(bikerService.getSingleBiker(id));
     }
 
     @Operation(description = "GET endpoint for retrieving an a report of the system's performance." +
             "\n\n Can only be done by admins.", summary = "Get a report of the system as a whole")
-    @GetMapping("/analysis")
-    @PreAuthorize("hasAuthority('" + Roles.ADMIN + "')")
-    public SystemAnalysis AnalyzeSystemPerformance() {
+    @GetMapping()
+    @PreAuthorize("hasAuthority('" + Roles.BACK_OFFICE + "')")
+    public SystemAnalysis analyzeSystemPerformance() {
         return analysisService.getSystemAnalysis(bikerService.getAllBikers());
     }
 
-    @Operation(description = "GET endpoint for generating suggestions for system improvements." +
+    @Operation(description = "GET endpoint for generating a system report with suggestions included." +
             "\n\n Can only be done by back office users.", summary = "Get suggestions for improving system")
-    @GetMapping("/improvements")
+    @GetMapping("/report")
     @PreAuthorize("hasAuthority('" + Roles.BACK_OFFICE + "')")
-    public SystemReport GenerateSuggestions() {
+    public SystemReport generateReport() {
         return analysisService.generateReport(bikerService.getAllBikers());
     }
 }
