09bd9f60e90a012b24f34bfd1073990072bf7c90
OEH808 <omarehababdelaziz8@gmail.com>
2024-04-03 10:33:46 +0200

    Cleaned up test import

--- src/test/java/io/biker/management/service/OrderHistoryServiceTest.java
+++ src/test/java/io/biker/management/service/OrderHistoryServiceTest.java
@@ -9,7 +9,6 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.Date;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
